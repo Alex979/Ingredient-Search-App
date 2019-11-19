afd8f8ea903f33b59e59b240c95fa1ccf39f6760
Alex Broaddus <alexbroaddus1@gmail.com>
2019-11-18 22:16:57 -0500

    Added camera functionality

--- app/src/main/java/com/example/ingredientsearch/MainActivity.kt
+++ app/src/main/java/com/example/ingredientsearch/MainActivity.kt
@@ -1,17 +1,117 @@
 package com.example.ingredientsearch
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var cameraButton: Button;
+    private lateinit var cameraButton: Button
+    private lateinit var imageView: ImageView
+    var currentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        cameraButton = findViewById(R.id.cameraButton);
+        cameraButton = findViewById(R.id.cameraButton)
+        imageView = findViewById(R.id.imageView)
+
+        cameraButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                takePicture()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    200
+                )
+            }
+
+        }
+    }
+
+    private fun takePicture() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    Log.d("DEBUG", "taking photo")
+                    createImageFile()
+                } catch (ex: IOException) {
+                    Log.d("DEBUG", "oof")
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.ingredientsearch.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, 1)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 200) {
+            // We only requested one permission, so its result is the first element
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("DEBUG", "Permission granted");
+                takePicture()
+            } else {
+                // User denied the permission :(
+                Log.d("DEBUG", "Permission denied");
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            imageView.setImageURI(Uri.parse(currentPhotoPath))
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        Log.d("DEBUG", "creating image")
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
     }
 }
